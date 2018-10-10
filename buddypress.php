14f19394ea274ebdc0fff47865cd412369f3208b
WPGuru4u <wpguru4u@gmail.com>
2018-10-10 06:09:09 -0400

    Add basic buddypress template support

---
+++ buddypress.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying BuddyPress pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.buddypress.org/themes/
+ *
+ * @package acorn
+ */
+
+get_header(); 
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+ 
+		<?php
+		while ( have_posts() ) :
+			the_post(); ?>
+
+			<article <?php post_class(); ?>>
+				<div class="entry-content container">
+
+					<?php 
+					the_content(); ?>
+
+				</div><!-- .entry-content -->
+			</article><!-- .bp_* -->
+
+		<?php
+		endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
