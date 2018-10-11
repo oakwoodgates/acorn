9f611f596f8b044656ed6134185fa609555cb004
WPGuru4u <wpguru4u@gmail.com>
2018-10-11 05:42:54 -0400

    Add docs

--- inc/variables.php
+++ inc/variables.php
@@ -1,11 +1,16 @@
 <?php 
 
 class Acorn_Vars {
-    
-    private static $some_var = 'something';
 
-    public static function get_some_var() {
-        return apply_filters( 'acorn_vars_some_var', self::$some_var );
-    }
+    /**
+     * Basic usage
+     *
+     *  private static $some_var = 'something';
+     *
+     *  public static function get_some_var() {
+     *      // Allow variable to be modified
+     *      return apply_filters( 'acorn_vars_some_var', self::$some_var );
+     *  }
+     */
 
 }
