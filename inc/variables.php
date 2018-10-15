bb7922bb7fadf7d3c319f4f7f7141d05a65d6722
WPGuru4u <wpguru4u@gmail.com>
2018-10-15 08:21:25 -0400

    Add full or standard header inner container

--- inc/variables.php
+++ inc/variables.php
@@ -16,7 +16,9 @@ class Acorn_Vars {
 
 	protected static $single_instance = null;
 
-	private static $buddypress_container_wrapper;
+	private static $header_is_full_width;
+
+	private static $buddypress_has_container_wrapper;
 	private static $buddypress_is_content_full_width;
 	private static $buddypress_content_container;
 
@@ -28,13 +30,22 @@ class Acorn_Vars {
 	}
 
 	protected function __construct() {
-		self::set_buddypress_container_wrapper();
-		self::set_buddypress_is_content_full_width();
-	}
-	
 
-	private static function set_buddypress_container_wrapper() {
-		self::$buddypress_container_wrapper = apply_filters( 'acorn_vars_buddypress_container_wrapper', false );
+		self::set_header_is_full_width();
+
+		// set BuddyPress variables
+		if ( class_exists( 'BuddyPress' ) && is_buddypress() ) {
+			self::set_buddypress_has_container_wrapper();
+			self::set_buddypress_is_content_full_width();
+		}
+	}
+
+	private static function set_header_is_full_width() {
+		self::$header_is_full_width = apply_filters( 'acorn_vars_header_is_full_width', true );
+	}
+
+	private static function set_buddypress_has_container_wrapper() {
+		self::$buddypress_has_container_wrapper = apply_filters( 'acorn_vars_buddypress_has_container_wrapper', true );
 	}
 
 	public static function set_buddypress_is_content_full_width() {
@@ -42,16 +53,30 @@ class Acorn_Vars {
 	}
 
 
-
+	public static function get_header_container() {
+		if ( self::$header_is_full_width ) {
+			$container = 'container-fluid';
+		} else {
+			$container = 'container';
+		}
+		return apply_filters( 'acorn_vars_get_header_container', $container );
+	}
 
 	public static function get_buddypress_container_wrapper() {
 
-		if ( self::$buddypress_container_wrapper ) {
-			$wrapper = 'container px-0';
+		if ( ! ( bp_is_user() || bp_is_group() ) ) {
+			if ( self::$buddypress_is_content_full_width ) {
+				$wrapper = 'container-fluid';
+			} else {
+				$wrapper = 'container';
+			}
 		} else {
-			$wrapper = '';
+			if ( self::$buddypress_has_container_wrapper ) {
+				$wrapper = 'container px-0';
+			} else {
+				$wrapper = '';
+			}
 		}
-
 		return apply_filters( 'acorn_vars_get_buddypress_container_wrapper', $wrapper );
 	}
 
--- template-parts/header.php
+++ template-parts/header.php
@@ -1,7 +1,7 @@
 <header class="banner">
 	<a class="skip-link screen-reader-text sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'acorn' ); ?></a>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container">
+		<div class="px-0 <?php echo Acorn_Vars::get_header_container(); ?>">
 			<?php the_custom_logo(); ?>
 
 			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
