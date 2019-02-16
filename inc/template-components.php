89bb6073b515cfa20ff7547e02cb4a6be5c182a2
WPGuru4u <wpguru4u@gmail.com>
2019-02-16 02:18:08 -0500

    Add template component for badges

--- inc/template-components.php
+++ inc/template-components.php
@@ -1,1 +1,22 @@
 <?php 
+
+function acorn_badge( $content, $link = '', $variation = '', $pill = false, $classes = '' ) {
+
+	if ( $classes ) {
+		$classes = 'badge ' . $classes;
+	} else {
+		$classes = 'badge';
+	}
+
+	if ( $pill ) 
+		$classes .= ' badge-pill';
+
+	if ( $variation ) 
+		$classes .= ' badge-' . $variation;
+
+	if ( $link ) {
+		echo '<a href="#" class="' . $classes . '">' . $content . '</a> ';
+	} else {
+		echo '<span class="' . $classes . '">' . $content . '</span> ';
+	}
+}
