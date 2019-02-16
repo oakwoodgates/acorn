637b112c9434372d3a236310813dc789b3719979
WPGuru4u <wpguru4u@gmail.com>
2019-02-16 01:15:55 -0500

    Add function to get post terms data in array

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -148,3 +148,24 @@ if ( ! function_exists( 'acorn_post_thumbnail' ) ) :
 		endif; // End is_singular().
 	}
 endif;
+
+function acorn_get_the_terms_array( $id = 0, $taxonomy = 'post_tag' ) {
+	$terms = get_the_terms( $id, $taxonomy );
+
+	if ( is_wp_error( $terms ) || empty( $terms ) )
+		return array();
+
+	$array = array();
+
+	foreach ( $terms as $term ) {
+		$link = get_term_link( $term, $taxonomy );
+		if ( ! is_wp_error( $link ) ) {
+			$array[] = array(
+				'url' => esc_url( $link ),
+				'name' => $term->name
+			);
+		}
+	}
+
+	return $array;
+}
